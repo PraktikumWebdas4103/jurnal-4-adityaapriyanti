879e98fc1a99d5249798dfb3f4a38d34d5b1d05d
adityaapriyanti <43569097+adityaapriyanti@users.noreply.github.com>
2018-09-25 16:54:42 +0700

    Add files via upload

--- proses.php
+++ proses.php
@@ -1,4 +1,21 @@
+<form method="POST" action="" enctype="multipart/form-data">
+
+	Nama : <input type="text" name="nama"><br>
+	Hobby : <br>
+	<input type="checkbox" name="hobbi[]">Nari</br>
+	<input type="checkbox" name="hobbi[]">Berenang</br>
+	<input type="checkbox" name="hobbi[]">Futsal</br>
+	<input type="checkbox" name="hobbi[]">Sepeda</br>
+	Gambar : 
+	<input type="file" name="gambar"></br><br>
+	<input type="submit" name="submit">
+</form>
+
 <?php
-
-
+	if (isset($_POST['submit'])) {
+		$gambar=$_FILES['gambar']['name'];
+		$hobby=$_POST['hobbi'];
+		echo "<img src='gambar'>";
+		
+	}
 ?>
